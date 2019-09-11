9351826a4c301b6acfd75a87b5b40646c1b26846
inwsy <inwsy@hotmail.com>
2019-09-11 17:07:37 +0800

    init

---
+++ spring-boot-quick-start/src/test/java/com/springboot/springbootquickstart/SpringBootQuickStartApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot.springbootquickstart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootQuickStartApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
