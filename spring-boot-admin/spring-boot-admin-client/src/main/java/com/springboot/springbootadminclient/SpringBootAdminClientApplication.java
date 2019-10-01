62da0726e32f67ff52950e4c9c6b51c9e74eab64
inwsy <inwsy@hotmail.com>
2019-10-01 22:36:58 +0800

    修改部分代码

--- spring-boot-admin/spring-boot-admin-client/src/main/java/com/springboot/springbootadminclient/SpringBootAdminClientApplication.java
+++ spring-boot-admin/spring-boot-admin-client/src/main/java/com/springboot/springbootadminclient/SpringBootAdminClientApplication.java
@@ -2,8 +2,10 @@ package com.springboot.springbootadminclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringBootAdminClientApplication {
 
     public static void main(String[] args) {
