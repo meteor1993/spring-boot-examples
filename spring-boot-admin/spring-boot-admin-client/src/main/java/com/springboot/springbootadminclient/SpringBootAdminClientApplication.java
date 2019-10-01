ea656f19b531b9a77f11d49e1ded175ef36568b7
inwsy <inwsy@hotmail.com>
2019-10-01 21:46:14 +0800

    + spring-boot-admin 示例代码

---
+++ spring-boot-admin/spring-boot-admin-client/src/main/java/com/springboot/springbootadminclient/SpringBootAdminClientApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootadminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminClientApplication.class, args);
+    }
+
+}
