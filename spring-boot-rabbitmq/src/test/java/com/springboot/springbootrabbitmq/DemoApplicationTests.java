99f8e8df964b9e885697afa7c7c59dd5a7004ca2
inwsy <inwsy@hotmail.com>
2019-10-08 16:42:28 +0800

    完善 spring-boot-rabbitmq 示例代码

--- spring-boot-rabbitmq/src/test/java/com/springboot/springbootrabbitmq/DemoApplicationTests.java
+++ spring-boot-rabbitmq/src/test/java/com/springboot/springbootrabbitmq/DemoApplicationTests.java
@@ -1,7 +1,14 @@
 package com.springboot.springbootrabbitmq;
 
+import com.springboot.springbootrabbitmq.fanout.FanoutSend;
+import com.springboot.springbootrabbitmq.many.SimpleManySend;
+import com.springboot.springbootrabbitmq.many.SimpleManySend1;
+import com.springboot.springbootrabbitmq.simple.SimpleSend;
+
+import com.springboot.springbootrabbitmq.topic.TopicSend;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +16,54 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    SimpleSend simpleSend;
+
+    @Autowired
+    SimpleManySend simpleManySend;
+
+    @Autowired
+    SimpleManySend1 simpleManySend1;
+
+    @Autowired
+    TopicSend topicSend;
+
+    @Autowired
+    FanoutSend fanoutSend;
+
+    @Test
+    public void simpleSend() {
+        simpleSend.send();
+    }
+
+    @Test
+    public void simpleOneSend() {
+        for (int i = 0; i < 100; i ++) {
+            simpleManySend.send(i);
+        }
+    }
+
+    @Test
+    public void simpleManySend() {
+        for (int i = 0; i < 100; i ++) {
+            simpleManySend.send(i);
+            simpleManySend1.send(i);
+        }
+    }
+
+    @Test
+    public void topicSend1() {
+        topicSend.send1();
+    }
+
+    @Test
+    public void topicSend2() {
+        topicSend.send2();
+    }
+
+    @Test
+    public void fanoutSend() {
+        fanoutSend.send();
+    }
 
 }
