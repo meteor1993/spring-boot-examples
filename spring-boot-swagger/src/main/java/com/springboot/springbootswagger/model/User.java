29e9f363576f87a8a582f936ad8b50fa4774497e
inwsy <inwsy@hotmail.com>
2019-10-11 01:11:19 +0800

    修改部分代码

--- spring-boot-swagger/src/main/java/com/springboot/springbootswagger/model/User.java
+++ spring-boot-swagger/src/main/java/com/springboot/springbootswagger/model/User.java
@@ -17,12 +17,12 @@ import java.util.Date;
 @NoArgsConstructor
 @ApiModel(value = "用户演示类", description = "请求参数类")
 public class User {
-    @ApiModelProperty(example = "1")
+    @ApiModelProperty(example = "1", notes = "用户ID")
     private Long id;
-    @ApiModelProperty(example = "geekdigging")
+    @ApiModelProperty(example = "geekdigging", notes = "用户名")
     private String nickName;
-    @ApiModelProperty(example = "1570689455000")
+    @ApiModelProperty(example = "1570689455000", notes = "创建时间")
     private Date createDate;
-    @ApiModelProperty(example = "18")
+    @ApiModelProperty(example = "18", notes = "用户年龄")
     private Integer age;
 }
