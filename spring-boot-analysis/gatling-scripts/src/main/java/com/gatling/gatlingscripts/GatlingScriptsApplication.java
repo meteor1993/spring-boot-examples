0e3fc8080c077bc0c883733f88a4d355bc92330f
inwsy <inwsy@hotmail.com>
2019-10-14 17:03:22 +0800

    增加 springboot 压力测试示例

---
+++ spring-boot-analysis/gatling-scripts/src/main/java/com/gatling/gatlingscripts/GatlingScriptsApplication.java
@@ -0,0 +1,13 @@
+package com.gatling.gatlingscripts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatlingScriptsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatlingScriptsApplication.class, args);
+    }
+
+}
